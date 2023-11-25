96d559485977f0aa2886abb9e3283a5911766bfb
Kamel CHATTI <chattikamel@gmail.com>
2023-11-25 01:05:55 +0100

    initiation du modèle

---
+++ model/src/test/java/StructureTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class StructureTest {
+
+    @Test
+    void test() {
+        Structure structure = new Structure();
+        assertNotNull(structure);
+    }
+}
