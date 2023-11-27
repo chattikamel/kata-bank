6ffb7bacc026e5e686a29f615400104a045604a9
Kamel CHATTI <chattikamel@gmail.com>
2023-11-27 11:46:23 +0100

    feat(api): consultation du solde

--- api/src/main/java/org/kbank/account/api/CompteApi.java
+++ api/src/main/java/org/kbank/account/api/CompteApi.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -18,17 +19,24 @@ public class CompteApi {
 
     @PostMapping(value = "/operations/{identifiant}", produces = MediaType.APPLICATION_JSON_VALUE)
     public void ajouterOperation(@PathVariable("identifiant") final String identifiant, @RequestBody Operation operation) {
-        Compte compte = new Compte(identifiant, apiCompteRepository);
+        Compte compte = new Compte(apiCompteRepository, identifiant);
         compte.traiter(toOperationModel(operation));
     }
 
     @ResponseBody
     @GetMapping(value = "/operations/{identifiant}", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Operation> listerOperations(@PathVariable("identifiant") final String identifiant) {
-        return new Compte(identifiant, apiCompteRepository)
+        return new Compte(apiCompteRepository, identifiant)
                 .listerOperations()
                 .map(toOperationApi)
                 .collect(Collectors.toList());
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/solde/{identifiant}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public BigDecimal recupererSolde(@PathVariable("identifiant") final String identifiant) {
+        return new Compte(apiCompteRepository, identifiant)
+                .solde();
     }
 
     private static Function<org.kbank.account.model.Operation, Operation> toOperationApi = op -> Operation.builder()
